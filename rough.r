d85f7e9a231a3ae87bfcfab8cbdd6685ef38f933
suhishan <suhishanbhandari@gmail.com>
2025-08-15 21:50:37 +0545

    Cleaned and Created Household-related variables, work-hours related variables and statuses of currently employed

--- rough.r
+++ rough.r
@@ -6,7 +6,30 @@
 
 # Testing out left_merge
 
+d1 %>% group_by(currently_emp_wage) %>% count()
+d1 %>% filter(is.na(currently_emp_wage)) %>% group_by(age) %>% count()
+d2 %>% group_by(dname) %>% count()
+d1 %>% filter(total_hours == 0) %>% count()
+d1 %>% count(is.na(selfemp_hours))
+hist(d1$total_hours)
+x <- tibble(
+  a = rep(c(1, 2, 3, NA), 100)
+)
 
+x <-  x %>% mutate(
+  yes = if_else(a !=2, 0, 1, missing = NA)
+)
 
+a <- "Suhishan"
 
+str_sub(str_to_lower(a), start = 1, end = 4)
 
+c <- cbind(x1 = c(1:3, NA), x2 = c(NA, 2, 3, NA))
+c <- tibble(
+  x1 = c(1:3, NA),
+  x2 = c(NA, 2, 3, NA)
+)
+
+c %>% mutate(
+  sum  = rowSums(select(., x1,x2), na.rm = T)
+)
